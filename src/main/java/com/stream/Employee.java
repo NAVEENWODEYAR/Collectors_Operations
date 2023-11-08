9627f4d25275132d5c88e59f45c872602009a684
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-08 19:00:04 +0530

    seters and getters,

--- src/main/java/com/stream/Employee.java
+++ src/main/java/com/stream/Employee.java
@@ -9,5 +9,65 @@ public class Employee
     String department;
     int yearOfJoining;
     double salary;
+    
+    // seters and getters,
+		public int getId() 
+		{
+			return id;
+		}
+		public void setId(int id) 
+		{
+			this.id = id;
+		}
+		public String getName() 
+		{
+			return name;
+		}
+		public void setName(String name) 
+		{
+			this.name = name;
+		}
+		public int getAge() 
+		{
+			return age;
+		}
+		public void setAge(int age) 
+		{
+			this.age = age;
+		}
+		public String getGender() 
+		{
+			return gender;
+		}
+		public void setGender(String gender) 
+		{
+			this.gender = gender;
+		}
+		public String getDepartment() 
+		{
+			return department;
+		}
+		public void setDepartment(String department)
+		{
+			this.department = department;
+		}
+		public int getYearOfJoining() 
+		{
+			return yearOfJoining;
+		}
+		public void setYearOfJoining(int yearOfJoining) 
+		{
+			this.yearOfJoining = yearOfJoining;
+		}
+		public double getSalary() 
+		{
+			return salary;
+		}
+		public void setSalary(double salary) 
+		{
+			this.salary = salary;
+		}
+	    
+    
      
 }
