b785aaddafaf83255a4a888e097217b72d644d92
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-09 12:29:14 +0530

    GM

--- src/main/java/com/stream/EmployeeImpl.java
+++ src/main/java/com/stream/EmployeeImpl.java
@@ -16,6 +16,7 @@ public class EmployeeImpl
 		List<Employee> employeeList = new ArrayList<>();
 		
 		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
 		employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
 		employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
 		employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
