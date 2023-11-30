91ee92af2ba8f7abe8250fd9968b88e633aabe04
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-30 21:22:44 +0530

    Collectors is a final class that extends the Object class

---
+++ src/main/java/com/stream/collectors/ReductionOperations.java
@@ -0,0 +1,15 @@
+package com.stream.collectors;
+
+public class ReductionOperations 
+{
+	static
+	{
+		System.out.println("\n Collectors is a final class that extends the Object class");
+	}
+	
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
