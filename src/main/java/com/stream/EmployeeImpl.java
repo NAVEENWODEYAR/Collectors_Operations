7f673a40ca7eca297d02cca2ae42f29fa62d2069
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-06 22:36:13 +0530

    streamAPI operations,

--- src/main/java/com/stream/EmployeeImpl.java
+++ src/main/java/com/stream/EmployeeImpl.java
@@ -13,11 +13,12 @@ public class EmployeeImpl
 
 	public static void main(String[] args) 
 	{
-		// List to store the values.,
+		// ArrayList to store the values.,
 		List<Employee> employeeList = new ArrayList<>();
 		
 		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
 		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+		employeeList.add(new Employee(111, "Brein", 59, "Female", "HR", 2019, 25000.0));
 		employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
 		employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
 		employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
